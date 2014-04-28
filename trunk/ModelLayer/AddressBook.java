2fb51b348b7607fa61c05c279b8411c5347122ea
lau.kondrup@gmail.com <lau.kondrup@gmail.com@8b0b6c67-b000-e9c7-bf1f-522c40e38827>
2014-04-28 11:08:15 +0000

    Person og AddressBook ver 0.1

--- trunk/ModelLayer/AddressBook.java
+++ trunk/ModelLayer/AddressBook.java
@@ -5,8 +5,8 @@ import java.util.ArrayList;
 /**
  * Write a description of class AddressBook here.
  * 
- * @author (your name) 
- * @version (a version number or a date)
+ * @author Group 3
+ * @version 0.1
  */
 public class AddressBook
 {
@@ -16,7 +16,7 @@ public class AddressBook
     /**
      * Constructor for objects of class AddressBook
      */
-    private AddressBook(){
+    public AddressBook(){
         persons = new ArrayList<Person>();
     }
 
@@ -28,4 +28,33 @@ public class AddressBook
         
         return instance;
     }
+    
+    public void createPerson(String name, String address, int postalCode, String city, String phone){
+        addPerson(new Person(name, address, postalCode, city, phone));
+    }
+    
+    public void deletePerson(Person person){
+        persons.remove(person);
+    }
+    
+    private void addPerson(Person person){
+        persons.add(person);
+    }
+    
+    public Person getPerson(int id){
+        Person returnPerson = null;
+        
+        for(Person p : persons){
+            if(p.getID() == id){
+                returnPerson = p;
+            }
+        }
+        
+        return returnPerson;
+    }
+    
+    public ArrayList<Person> getAllPersons(){
+        return persons;
+    }
+    
 }
