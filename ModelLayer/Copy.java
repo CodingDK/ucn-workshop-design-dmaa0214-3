488453f6ce50f849fe2c90b6ca170cdb54a2e5b1
lau.kondrup <lau.kondrup@gmail.com@8b0b6c67-b000-e9c7-bf1f-522c40e38827>
2014-04-29 08:01:39 +0000

    Keine extends i Copy

--- ModelLayer/Copy.java
+++ ModelLayer/Copy.java
@@ -7,7 +7,7 @@ package ModelLayer;
  * @author Group 3
  * @version (a version number or a date)
  */
-public class Copy extends DVDContainer
+public class Copy
 {
     private int serialNumber;
     private String purchaseDate;
