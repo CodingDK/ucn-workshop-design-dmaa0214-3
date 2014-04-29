a347294256eb4862d56472d5959e5f4190932fe3
zoinoz <zoinoz@8b0b6c67-b000-e9c7-bf1f-522c40e38827>
2014-04-29 17:58:27 +0000

    AddressCtrTest oprettet og testet. DVDCtrTest oprettet

---
+++ trunk/ControlLayer/DVDCtrTest.java
@@ -0,0 +1,50 @@
+package ControlLayer;
+
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class DVDCtrTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class DVDCtrTest
+{
+    /**
+     * Default constructor for test class DVDCtrTest
+     */
+    public DVDCtrTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+
+    @Test
+    public void DVDTest()
+    {
+    }
+}
+
